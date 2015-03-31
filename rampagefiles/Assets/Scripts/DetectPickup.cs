d9d5948cf96c94706b60026f99a39ea444fc7d14
RampageGames <www.chris.hill@gmail.com>
2015-03-31 22:59:51 +1300

    weapon spawn and switching

---
+++ rampagefiles/Assets/Scripts/DetectPickup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectPickup : MonoBehaviour {
+
+	private bool triggered;
+	private float respawn = 10f;
+	private GameObject childObject;
+
+	// Use this for initialization
+	void Start () {
+
+		triggered = false;
+		childObject = transform.GetChild (0).gameObject;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (respawn <= 0) {
+			triggered = false;
+			collider.enabled = true;
+			childObject.SetActive(true);
+			respawn = 5f;
+		}
+		if (triggered) {
+			collider.enabled = false;
+			respawn -= Time.deltaTime;
+			childObject.SetActive(false);
+		}
+
+	}
+
+	void OnTriggerEnter(Collider col){
+		triggered = true;
+	}
+}
